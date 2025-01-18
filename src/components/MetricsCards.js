55159ac811ecc9d092cabda7a100908bb5fa91be
nusharak <82564479+nusharak@users.noreply.github.com>
2025-01-18 17:14:00 +0530

    commit fixed2

--- src/components/MetricsCards.js
+++ src/components/MetricsCards.js
@@ -6,7 +6,7 @@ export const MetricsCards = ({ filteredData }) => {
     totals.totalCost += item[' Total Cost '] || 0;
     totals.totalSales += item[' Gross Sales '] || 0;
     totals.netPL += item['Net P/L'] || 0;
-    totals.mailed += item['mailed'] || 0;
+    totals.mailed += item['Mailed'] || 0;
     totals.refunds += item[' Refunds '] || 0;
     totals.shippedQty += item['Ship Qty'] || 0;
     totals.grossOrders += item['Gross Orders'] || 0;
@@ -36,25 +36,25 @@ export const MetricsCards = ({ filteredData }) => {
       <Grid item xs={12} md={3}>
         <Box p={2} bgcolor="#f0f0f0" borderRadius="4px">
           <Typography variant="body1">Total Refunds:</Typography>
-          <Typography variant="h6">{totalSums.refunds}</Typography>
+          <Typography variant="h6">{parseFloat(totalSums.refunds).toFixed(2)}</Typography>
         </Box>
       </Grid>
       <Grid item xs={12} md={3}>
         <Box p={2} bgcolor="#f0f0f0" borderRadius="4px">
           <Typography variant="body1">Total Cost:</Typography>
-          <Typography variant="h6">{totalSums.totalCost}</Typography>
+          <Typography variant="h6">{parseFloat(totalSums.totalCost).toFixed(2)}</Typography>
         </Box>
       </Grid>
       <Grid item xs={12} md={3}>
         <Box p={2} bgcolor="#f0f0f0" borderRadius="4px">
           <Typography variant="body1">Total Sales:</Typography>
-          <Typography variant="h6">{totalSums.totalSales}</Typography>
+          <Typography variant="h6">{parseFloat(totalSums.totalSales).toFixed(2)}</Typography>
         </Box>
       </Grid>
       <Grid item xs={12} md={3}>
         <Box p={2} bgcolor="#f0f0f0" borderRadius="4px">
           <Typography variant="body1">Total Net P/L:</Typography>
-          <Typography variant="h6">{totalSums.netPL}</Typography>
+          <Typography variant="h6">{parseFloat(totalSums.netPL).toFixed(2)}</Typography>
         </Box>
       </Grid>
     </>
